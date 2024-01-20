23f3e6926d5039e4cdac9f0240662d9972c9241c
maxwelltyreece <107719479+maxwelltyreece@users.noreply.github.com>
2024-01-20 20:52:13 +0000

    ESLint

---
+++ CloudChasers/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/style-prop-object': [
+      'error',
+      {
+        allow: ['StatusBar'],
+      },
+    ],
+  },
+};
