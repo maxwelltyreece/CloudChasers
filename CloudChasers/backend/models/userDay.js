ef87ecea9cc6ac176e07985073f1f2ffa0d370ea
Matt-JL-Burton <mslburton24@yahoo.com>
2024-02-03 15:30:18 +0000

    feat : add foodItem & recipeItem models
    also reformat code belonging to the existing models a tiny amount by removing the unneccessary list around them

--- CloudChasers/backend/models/userDay.js
+++ CloudChasers/backend/models/userDay.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const userDaySchema = new mongoose.Schema({
 	date : { type: Date, required: true },
-    userId : [ 
-        { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }
-    ],
+    userId : { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true},
 });
 
 module.exports = mongoose.model('userDay', userDaySchema);
--- CloudChasers/backend/models/userDayItem.js
+++ CloudChasers/backend/models/userDayItem.js
@@ -2,15 +2,9 @@ const mongoose = require('mongoose');
 
 const userDayItemSchema = new mongoose.Schema({
     date : { type: Date, required: true },
-    foodItemID : [ 
-        { type: mongoose.Schema.Types.ObjectId, ref: 'foodItem', required: true }
-    ],
-    receipeID: [ 
-        { type: mongoose.Schema.Types.ObjectId, ref: 'receipe', required: true }
-    ],
-    userDayID : [ 
-        { type: mongoose.Schema.Types.ObjectId, ref: 'userDay', required: true }
-    ],
+    foodItemID : { type: mongoose.Schema.Types.ObjectId, ref: 'foodItem', required: true },
+    receipeID: { type: mongoose.Schema.Types.ObjectId, ref: 'receipe', required: true },
+    userDayID : { type: mongoose.Schema.Types.ObjectId, ref: 'userDay', required: true },
 });
 
 module.exports = mongoose.model('userDayItem', userDayItemSchema);
