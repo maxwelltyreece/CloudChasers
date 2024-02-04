e5d6407b6a48d8317cf7e9ead413e04c1559e642
Matt-JL-Burton <mslburton24@yahoo.com>
2024-02-04 14:27:03 +0000

    feat : add functionality to write jest cases to project

---
+++ CloudChasers/backend/tests/userController.test.js
@@ -0,0 +1,6 @@
+const userController = require('../controllers/userController');
+const userModel = require('../models/user');
+
+test('userController.getUserById', () => {
+    expect(userController.getUserById).toBe(userModel.getUserById);
+});
