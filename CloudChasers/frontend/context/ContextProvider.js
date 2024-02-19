4bf7f52a8b57599ffdccd0f1077a2030991427a5
Sam Thomas <samuel.mmthomas@gmail.com>
2024-02-19 12:27:30 +0000

    docs: add JSDoc comments to Context files

--- CloudChasers/frontend/context/ContextProvider.js
+++ CloudChasers/frontend/context/ContextProvider.js
@@ -1,3 +1,13 @@
+/**
+ * ContextProvider component.
+ *
+ * This is a higher-order component that wraps its children with the UserProvider.
+ * This allows all child components to have access to the Contexts it contains.
+ *
+ * @param {Object} props The properties passed to the component.
+ * @param {React.ReactNode} props.children The child components to be wrapped by the UserProvider.
+ * @returns {React.Element} The ContextProvider component that wraps its children with the UserProvider.
+ */
 import React from 'react';
 import { UserProvider } from './UserContext';
 
