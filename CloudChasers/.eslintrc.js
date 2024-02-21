19e6da0f6129b072f48a9e6d4cc0c56bfef9eade
maxwelltyreece <107719479+maxwelltyreece@users.noreply.github.com>
2024-02-21 12:27:07 +0000

    feat: eslint settings update

--- CloudChasers/.eslintrc.js
+++ CloudChasers/.eslintrc.js
@@ -1,38 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/style-prop-object': [
-      'error',
-      {
-        allow: ['StatusBar'],
-      },
-    ],
-    'no-tabs': ['error',
-      { allowIndentationTabs: true }],
-    indent: ['warn', 'tab'],
-    'no-unused-vars': ['warn'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'airbnb',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': 'off',
+		'react/style-prop-object': ['error', { allow: ['StatusBar'] }],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		indent: ['warn', 'tab'],
+		'no-unused-vars': ['warn'],
+		'linebreak-style': 'off',
+	},
 };
