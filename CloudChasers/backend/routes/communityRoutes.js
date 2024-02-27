001ebe66ae7d6567f071f9fd78bda8e0a120cb0d
Benjy Donoff <benjamin@donoff.com>
2024-02-27 12:54:17 +0000

    feat: add new community features: get community members, get admin status, get all communities

--- CloudChasers/backend/routes/communityRoutes.js
+++ CloudChasers/backend/routes/communityRoutes.js
@@ -7,5 +7,8 @@ const communityController = require('../controllers/communityController');
 router.post('/create', userMiddleware, communityController.createCommunity);
 router.post('/join', userMiddleware, communityController.joinCommunity);
 router.get('/details', userMiddleware, communityController.getCommunityDetails);
+router.get('/members', userMiddleware, communityController.getCommunityMembers);
+router.get('/isAdmin', userMiddleware, communityController.getAdminStatus);
+router.get('/all', userMiddleware, communityController.getAllCommunities);
 
 module.exports = router;
