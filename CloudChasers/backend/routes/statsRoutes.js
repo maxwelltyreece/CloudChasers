297c32a27abe633a01fcaaec4e5c761956e74c73
TinySilverfish <tinysilverfish@gmail.com>
2024-03-07 01:15:15 +0000

    add(statsController): add all daily nutrient intake endpoint

--- CloudChasers/backend/routes/statsRoutes.js
+++ CloudChasers/backend/routes/statsRoutes.js
@@ -4,7 +4,14 @@ const router = express.Router();
 const userMiddleware = require('../middleware/userMiddleware');
 const statsController = require('../controllers/statsController');
 
-router.put('/streak', userMiddleware, statsController.getStreaks);
+router.post('/streak', userMiddleware, statsController.getStreaks);
 router.get('/daily-caloric-intake', userMiddleware, statsController.getDailyCaloricIntake);
+router.get('/daily-water-intake', userMiddleware, statsController.getDailyWaterIntake);
+router.get('/daily-protein-intake', userMiddleware, statsController.getDailyProteinIntake);
+router.get('/daily-carb-intake', userMiddleware, statsController.getDailyCarbIntake);
+router.get('/daily-fat-intake', userMiddleware, statsController.getDailyFatIntake);
+router.get('/daily-sugar-intake', userMiddleware, statsController.getDailySugarIntake);
+router.get('/daily-sodium-intake', userMiddleware, statsController.getDailySodiumIntake);
+router.get('/daily-fibre-intake', userMiddleware, statsController.getDailyFibreIntake);
 
 module.exports = router;
