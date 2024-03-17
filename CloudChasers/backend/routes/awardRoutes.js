12fe1a7dc038e6d512391fb809f79c5e640e98d8
Matt-JL-Burton <mslburton24@yahoo.com>
2024-03-17 14:53:41 +0000

    feat : Complete award controller

--- CloudChasers/backend/routes/awardRoutes.js
+++ CloudChasers/backend/routes/awardRoutes.js
@@ -5,6 +5,10 @@ const awardController = require('../controllers/awardController');
 const router = express.Router();
 
 router.post('/createAward', userMiddleware, awardController.createAward);
-router.get('/getAwards', userMiddleware, awardController.getAwards);
+router.get('/getAllAwards', userMiddleware, awardController.getAllAwards);
+router.get('/getAward', userMiddleware, awardController.getAward);
+router.post('/awardUser', userMiddleware, awardController.awardUser);
+router.get('/getUserAwards', userMiddleware, awardController.getUserAwards);
+router.get('/getAwardsToBeIssued', userMiddleware, awardController.getAwardsToBeIssued);
 
 module.exports = router;
