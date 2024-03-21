2865a81e43ddb7421dc6568273dcbd420746c55a
Emily Lazenby <124580748+elaz345@users.noreply.github.com>
2024-03-21 18:57:40 +0000

    fix: formatting modals

--- frontend/components/RecipeBox.js
+++ frontend/components/RecipeBox.js
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, Modal, Pressable } from 'react-native';
-import proptypes from 'prop-types';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Modal,
+  Pressable,
+  TouchableOpacity,
+  onClose,
+} from "react-native";
+import proptypes from "prop-types";
 
 const styles = StyleSheet.create({
   box: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
-    overflow: 'hidden', // Ensure the image corners are also rounded
-    width: '48%',
+    overflow: "hidden",
+    width: "48%",
     aspectRatio: 1,
     margin: 8,
   },
   image: {
-    position: 'absolute', // Position image absolute to cover the whole box
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover', // Cover the entire area of the box
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   titleContainer: {
     flex: 1,
-    justifyContent: 'flex-end', // Position the title at the bottom of the box
+    justifyContent: "flex-end",
     padding: 10,
   },
   title: {
     fontSize: 16,
-    fontFamily: 'Montserrat_700Bold',
-    color: 'white', // Ensure the title is visible on the image
-    textShadowColor: 'rgba(0, 0, 0, 0.75)', // Optional: Add shadow to text for better readability
+    fontFamily: "Montserrat_700Bold",
+    color: "white",
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
     textShadowOffset: { width: -1, height: 1 },
     textShadowRadius: 10,
   },
@@ -55,15 +64,33 @@ const styles = StyleSheet.create({
   },
   modalImage: {
     width: 250,
-    height: 250, 
-    resizeMode: 'contain', 
+    height: 250,
+    resizeMode: "contain",
   },
-  
+  text: {
+    fontSize: 20,
+    marginBottom: 15,
+    textAlign: "center",
+    fontFamily: "Montserrat_700Bold",
+  },
+  closeText: {
+    fontSize: 24,
+    color: "black",
+    fontWeight: "bold",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 3,
+    right: 2,
+    padding: 10,
+    zIndex: 1,
+  },
 });
 
 function RecipeBox({ title }) {
   const [modalVisible, setModalVisible] = useState(false);
-  const imageUrl = "https://firebasestorage.googleapis.com/v0/b/gobl-b4e3d.appspot.com/o/Recipe_Pictures%2F65fc78d19e672bbcbb30ca4b.jpg?alt=media&token=0d6af43d-3343-49b4-bc5f-abd74f5bca4d";
+  const imageUrl =
+    "https://firebasestorage.googleapis.com/v0/b/gobl-b4e3d.appspot.com/o/Recipe_Pictures%2F65fc78d19e672bbcbb30ca4b.jpg?alt=media&token=0d6af43d-3343-49b4-bc5f-abd74f5bca4d";
 
   return (
     <Pressable style={styles.box} onPress={() => setModalVisible(true)}>
@@ -78,22 +105,25 @@ function RecipeBox({ title }) {
         visible={modalVisible}
         onRequestClose={() => {
           setModalVisible(!modalVisible);
-        }}>
+        }}
+      >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.text}>{title}</Text>
-            
-            <Image source={{ uri: imageUrl }} style={styles.modalImage} />
-            <Pressable onPress={() => setModalVisible(false)}>
-              <Text style={styles.closeText}>Close</Text>
+            <Pressable
+              onPress={() => setModalVisible(false)}
+              style={styles.closeButton}
+            >
+              <Text style={styles.closeText}>X</Text>
             </Pressable>
+
+            <Text style={styles.text}>{title}</Text>
+            <Image source={{ uri: imageUrl }} style={styles.modalImage} />
           </View>
         </View>
       </Modal>
     </Pressable>
   );
 }
-
 
 export default RecipeBox;
 
