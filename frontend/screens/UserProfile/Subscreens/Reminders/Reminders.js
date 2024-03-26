40f80a0fdd666f3131afd91953b69ef2d17fc9ad
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-03-26 14:02:44 +0000

    chore: jsDoc on userProfile

--- frontend/screens/UserProfile/Subscreens/Reminders/Reminders.js
+++ frontend/screens/UserProfile/Subscreens/Reminders/Reminders.js
@@ -1,16 +1,15 @@
-/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import {
-	View, Text, ScrollView, KeyboardAvoidingView, Modal, 
-	TextInput, Pressable, Alert, Platform
+    View, Text, ScrollView, KeyboardAvoidingView, Modal, 
+    TextInput, Pressable, Alert, Platform
 } from 'react-native';
 import { useReminders } from '../../../../contexts/RemindersContext';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { styles } from './styles';
 
-const frequencyOptions = ['Daily', 'Weekly']; // Options for the segmented control
-
+const FREQUENCY_OPTIONS = ['Daily', 'Weekly'];
+const STORAGE_KEY = 'REMINDERS';
 
 /**
  * Reminders is a screen component designed for displaying and managing the users created reminders.
@@ -18,125 +17,170 @@ const frequencyOptions = ['Daily', 'Weekly']; // Options for the segmented contr
  * @returns {React.Element} The rendered Reminders screen.
  */
 function Reminders() {
-	const { reminders, setReminders } = useReminders();
-	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [newReminder, setNewReminder] = useState({
-		id: null,
-		description: '',
-		time: '',
-		date: '',
-		frequency: 'Daily', // Default frequency
-	});
-	const [date, setDate] = useState(new Date());
-	const [showTimePicker, setShowTimePicker] = useState(Platform.OS === 'ios'); // iOS will always show, Android controlled by state
-	const [selectedFrequencyIndex, setSelectedFrequencyIndex] = useState(0);
+    const { reminders, setReminders } = useReminders();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [newReminder, setNewReminder] = useState({
+        id: null,
+        description: '',
+        time: '',
+        date: '',
+        frequency: FREQUENCY_OPTIONS[0],
+    });
+    const [date, setDate] = useState(new Date());
+    const [showTimePicker, setShowTimePicker] = useState(Platform.OS === 'ios'); // iOS will always show, Android controlled by state
+    const [selectedFrequencyIndex, setSelectedFrequencyIndex] = useState(0);
 
+    /**
+     * Toggles the visibility of action buttons for a reminder.
+     *
+     * @param {number} id - The id of the reminder.
+     */
+    const toggleActionButtonsVisibility = (id) => {
+        const updatedReminders = reminders.map((reminder) => 
+            reminder.id === id ? { ...reminder, showActions: !reminder.showActions } : reminder
+        );
+        setReminders(updatedReminders);
+    };
 
-	const toggleActionButtonsVisibility = (id) => {
-		const updatedReminders = reminders.map((reminder) => 
-			reminder.id === id ? { ...reminder, showActions: !reminder.showActions } : reminder
-		);
-		setReminders(updatedReminders);
-	};
+	/**
+     * Opens the modal and resets the new reminder details.
+     */
+    const openModalAndResetFields = () => {
+        setIsModalVisible(true);
+        setNewReminder({
+            id: null,
+            description: '',
+            time: '',
+            date: '',
+            frequency: FREQUENCY_OPTIONS[0],
+        });
+        setDate(new Date());
 
-	// Function to open the modal and reset the new reminder details
-	const openModalAndResetFields = () => {
-		setIsModalVisible(true);
-		// Resetting newReminder fields to their default values
-		setNewReminder({
-			id: null,
-			description: '',
-			time: '',
-			date: '',
-			frequency: 'Daily',
-		});
-		setDate(new Date());
+        if (Platform.OS === 'android') {
+            setShowTimePicker(false);
+        }
+    };
 
-		if (Platform.OS === 'android') {
-			setShowTimePicker(false);
-		}
-	};
+	/**
+     * Loads reminders from storage.
+     */
+    useEffect(() => {
+        const loadReminders = async () => {
+            try {
+                const storedReminders = await AsyncStorage.getItem(STORAGE_KEY);
+                if (storedReminders) setReminders(JSON.parse(storedReminders));
+            } catch (error) {
+                Alert.alert('Error', 'Failed to load the reminders.');
+            }
+        };
 
+        loadReminders();
+    }, []);
 
-	useEffect(() => {
-		const loadReminders = async () => {
-			try {
-				const storedReminders = await AsyncStorage.getItem('REMINDERS');
-				if (storedReminders) setReminders(JSON.parse(storedReminders));
-			} catch (error) {
-				// Alert.alert('Error', 'Failed to load the reminders.');
-			}
-		};
+	/**
+     * Saves reminders to storage.
+     */
+    useEffect(() => {
+        const saveReminders = async () => {
+            try {
+                await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(reminders));
+            } catch (error) {
+                Alert.alert('Error', 'Failed to save the reminders.');
+            }
+        };
 
-		loadReminders();
-	}, []);
+        saveReminders();
+    }, [reminders]);
 
-	useEffect(() => {
-		const saveReminders = async () => {
-			try {
-				await AsyncStorage.setItem('REMINDERS', JSON.stringify(reminders));
-			} catch (error) {
-				// Alert.alert('Error', 'Failed to save the reminders.');
-			}
-		};
+	/**
+     * Adds a new reminder or updates an existing one.
+     */
+    const handleAddReminder = () => {
+        if (!newReminder.description.trim() || !newReminder.time) {
+            Alert.alert('Missing Information', 'You cannot add a reminder without a description and a time.');
+            return;
+        }
 
-		saveReminders();
-	}, [reminders]);
+        const updatedReminders = newReminder.id
+            ? reminders.map((reminder) => (reminder.id === newReminder.id ? newReminder : reminder))
+            : [...reminders, { ...newReminder, id: Date.now() }];
 
-	const handleAddReminder = () => {
-		if (!newReminder.description.trim() || !newReminder.time) {
-			Alert.alert('Missing Information', 'You cannot add a reminder without a description and a time.');
-			return;
-		}
+        setReminders(updatedReminders);
+        setIsModalVisible(false);
+        setNewReminder({
+            id: null, description: '', time: '', date: '', frequency: FREQUENCY_OPTIONS[0],
+        });
+        setShowTimePicker(false);
+    };
 
-		const updatedReminders = newReminder.id
-			? reminders.map((reminder) => (reminder.id === newReminder.id ? newReminder : reminder))
-			: [...reminders, { ...newReminder, id: Date.now() }]; // Use Date.now() for a unique id
+	/**
+     * Deletes a reminder.
+     *
+     * @param {number} id - The id of the reminder to delete.
+     */
+    const deleteReminder = (id) => {
+        const filteredReminders = reminders.filter((reminder) => reminder.id !== id);
+        setReminders(filteredReminders);
+    };
 
-		setReminders(updatedReminders);
-		setIsModalVisible(false);
-		setNewReminder({
-			id: null, description: '', time: '', date: '', frequency: 'Daily',
-		});
-		setShowTimePicker(false);
-	};
+	/**
+     * Edits a reminder.
+     *
+     * @param {number} id - The id of the reminder to edit.
+     */
+    const editReminder = (id) => {
+        const reminderToEdit = reminders.find((reminder) => reminder.id === id);
+        if (reminderToEdit) {
+            setNewReminder(reminderToEdit);
+            setIsModalVisible(true);
+            setDate(new Date());
+        }
+    };
 
-	const deleteReminder = (id) => {
-		const filteredReminders = reminders.filter((reminder) => reminder.id !== id);
-		setReminders(filteredReminders);
-	};
+	/**
+     * Handles the change of time.
+     *
+     * @param {Event} event - The event object.
+     * @param {Date} selectedDate - The selected date.
+     */
+    const onChangeTime = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        if (Platform.OS === 'android') {
+            setShowTimePicker(false);
+        }
+        setDate(currentDate);
 
-	const editReminder = (id) => {
-		const reminderToEdit = reminders.find((reminder) => reminder.id === id);
-		if (reminderToEdit) {
-			setNewReminder(reminderToEdit);
-			setIsModalVisible(true);
-			setDate(new Date());
-		}
-	};
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? `0${minutes}` : minutes} ${hours < 12 ? 'AM' : 'PM'}`;
+        setNewReminder({ ...newReminder, time: formattedTime });
+    };
 
-	const onChangeTime = (event, selectedDate) => {
-		const currentDate = selectedDate || date;
-		if (Platform.OS === 'android') {
-			setShowTimePicker(false);
-		}
-		setDate(currentDate);
+	/**
+     * Handles the change of frequency.
+     *
+     * @param {number} index - The index of the selected frequency.
+     */
+    const handleFrequencyChange = (index) => {
+        setSelectedFrequencyIndex(index);
+        const selectedFrequency = FREQUENCY_OPTIONS[index];
+        setNewReminder({ ...newReminder, frequency: selectedFrequency });
+    };
 
-		const hours = currentDate.getHours();
-		const minutes = currentDate.getMinutes();
-		const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? `0${minutes}` : minutes} ${hours < 12 ? 'AM' : 'PM'}`;
-		setNewReminder({ ...newReminder, time: formattedTime });
-	};
+	const IOS = 'ios';
+    const DONE = 'done';
+    const TIME = 'time';
+    const SPINNER = 'spinner';
+    const SLIDE = 'slide';
+    const HEIGHT = 'height';
+    const PADDING = 'padding';
+    const BOLD = 'bold';
+    const INLINE = 'inline';
+    const { description, time } = newReminder;
 
-	const handleFrequencyChange = (index) => {
-		setSelectedFrequencyIndex(index);
-		const selectedFrequency = frequencyOptions[index];
-		setNewReminder({ ...newReminder, frequency: selectedFrequency });
-	};
-
-	return (
-		<View style={styles.container}>
-			<ScrollView style={styles.scrollViewContainer}>
+    return (
+        <View style={styles.container}>
+            <ScrollView style={styles.scrollViewContainer}>
 				{reminders.map((reminder) => (
 					<View key={reminder.id} style={styles.reminderItem}>
 						<Text style={styles.remindersDescription} numberOfLines={4}>{reminder.description}</Text>
@@ -161,70 +205,62 @@ function Reminders() {
 							<Pressable onPress={() => deleteReminder(reminder.id)} style={styles.deleteButton}>
 								<Text style={styles.actionButtonText}>Delete</Text>
 							</Pressable>
-							{/* <Pressable onPress={() => toggleActionButtonsVisibility(reminder.id)} style={styles.backButton}>
-								<Text style={styles.backButtonText}>Back</Text>
-							</Pressable> */}
 						</View>
 						)}
 					</View>
 				))}
 			</ScrollView>
 
-			{/* <View style={styles.semiCircle}></View> */}
-
 			<Pressable style={styles.addReminderButton} onPress={openModalAndResetFields}>
-				<Text style={styles.addReminderButtonText}>
-						Add Reminder
-				</Text>
-			</Pressable>
+                <Text style={styles.addReminderButtonText}>Add Reminder</Text>
+            </Pressable>
 
 			<Modal
-				animationType="slide"
-				transparent
-				visible={isModalVisible}
-				onRequestClose={() => setIsModalVisible(false)}
-			>
-				<KeyboardAvoidingView
-					behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-					style={{ flex: 1 }}
-					keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 50}
-				>
-					<ScrollView>
-						<View style={styles.modalView}>
-							<TextInput
-								style={[styles.descriptionInput]}
-								placeholder="Description"
-								onChangeText={(text) => setNewReminder({ ...newReminder, description: text })}
-								value={newReminder.description}
-								blurOnSubmit
-								returnKeyType='done'
-							/>
+                animationType={SLIDE}
+                transparent
+                visible={isModalVisible}
+                onRequestClose={() => setIsModalVisible(false)}
+            >
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === IOS ? PADDING : HEIGHT}
+                    style={{ flex: 1 }}
+                    keyboardVerticalOffset={Platform.OS === IOS ? 0 : 50}
+                >
+                    <ScrollView>
+                        <View style={styles.modalView}>
+                            <TextInput
+                                style={[styles.descriptionInput]}
+                                placeholder="Description"
+                                onChangeText={(text) => setNewReminder({ ...newReminder, description: text })}
+                                value={description}
+                                blurOnSubmit
+                                returnKeyType={DONE}
+                            />
 
-							<Text style={{ fontSize: 16, fontWeight: 'bold' }}>Current Time Selected:</Text>
+                            <Text style={{ fontSize: 16, fontWeight: BOLD }}>Current Time Selected:</Text>
 
-							{(Platform.OS != 'ios' || !newReminder.time) && (
-								<Text style={styles.currentTimeSelectedText}>{newReminder.time || '(Choose Time)'}</Text>
-								)
-							}
+                            {(Platform.OS != IOS || !time) && (
+                                <Text style={styles.currentTimeSelectedText}>{time || '(Choose Time)'}</Text>
+                            )}
 
-							{Platform.OS === 'ios' || showTimePicker ? (
-								<DateTimePicker
-									value={date}
-									mode="time"
-									display={Platform.OS === 'ios' ? 'inline' : 'spinner'}
-									onChange={onChangeTime}
-									is24Hour={false}
-								/>
-								) : (
-								<Pressable onPress={() => setShowTimePicker(true)} style={styles.timePickerButton}>
-									<Text style={styles.timePickerButtonText}>Select Time</Text>
-								</Pressable>
-							)}
+                            {Platform.OS === IOS || showTimePicker ? (
+                                <DateTimePicker
+                                    value={date}
+                                    mode={TIME}
+                                    display={Platform.OS === IOS ? INLINE : SPINNER}
+                                    onChange={onChangeTime}
+                                    is24Hour={false}
+                                />
+                            ) : (
+                                <Pressable onPress={() => setShowTimePicker(true)} style={styles.timePickerButton}>
+                                    <Text style={styles.timePickerButtonText}>Select Time</Text>
+                                </Pressable>
+                            )}
 
-							<Text style={styles.selectFreqTitle}>Select Frequency: </Text>
+                            <Text style={styles.selectFreqTitle}>Select Frequency: </Text>
 
-							<View style={styles.frequencySelector}>
-								{frequencyOptions.map((option, index) => (
+                            <View style={styles.frequencySelector}>
+                                {FREQUENCY_OPTIONS.map((option, index) => (
 									<Pressable
 										key={option}
 										style={[
@@ -241,27 +277,27 @@ function Reminders() {
 							</View>
 
 							<View style={styles.actionButtonsSection}>
-								<Pressable style={styles.cancelButton} onPress={() => setIsModalVisible(false)}>
-									{({ pressed }) => (
-										<Text style={[styles.cancelButtonText, pressed ? styles.pressedText : {}]}>
-											Cancel
-										</Text>
-									)}
-								</Pressable>
-								<Pressable style={styles.addButton} onPress={handleAddReminder}>
-									{({ pressed }) => (
-										<Text style={[styles.addButtonText, pressed ? styles.pressedText : {}]}>
-											Done
-										</Text>
-									)}
-								</Pressable>
-							</View>
-						</View>
-					</ScrollView>
-				</KeyboardAvoidingView>
-			</Modal>
-		</View>
-	);
+                                <Pressable style={styles.cancelButton} onPress={() => setIsModalVisible(false)}>
+                                    {({ pressed }) => (
+                                        <Text style={[styles.cancelButtonText, pressed ? styles.pressedText : {}]}>
+                                            Cancel
+                                        </Text>
+                                    )}
+                                </Pressable>
+                                <Pressable style={styles.addButton} onPress={handleAddReminder}>
+                                    {({ pressed }) => (
+                                        <Text style={[styles.addButtonText, pressed ? styles.pressedText : {}]}>
+                                            Done
+                                        </Text>
+                                    )}
+                                </Pressable>
+                            </View>
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </Modal>
+        </View>
+    );
 }
 
 export default Reminders;
--- frontend/screens/UserProfile/UserProfile.js
+++ frontend/screens/UserProfile/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
-	View, Text, Image, FlatList, TouchableOpacity, ActivityIndicator,
+    View, Text, Image, FlatList, TouchableOpacity, ActivityIndicator,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,22 +12,32 @@ import axios from 'axios';
 import { getImageLink } from '../../services/ImageService';
 import { useFocusEffect } from '@react-navigation/native';
 
+/**
+ * UserProfile component. This component is responsible for rendering the user profile.
+ * @component
+ */
 function UserProfile() {
-	const navigation = useNavigation();
-	const { userDetails, updateUserDetails } = useUser();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigation = useNavigation();
+    const { userDetails, updateUserDetails } = useUser();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [imageLink, setImageLink] = useState('');
     const [imageLoaded, setImageLoaded] = useState(false);
 
-	useEffect(() => {
-		const verifyLoginStatus = async () => {
-			const token = await AsyncStorage.getItem('token');
-			setIsLoggedIn(!!token);
-		};
+    /**
+     * Effect hook to verify login status.
+     */
+    useEffect(() => {
+        const verifyLoginStatus = async () => {
+            const token = await AsyncStorage.getItem('token');
+            setIsLoggedIn(!!token);
+        };
 
-		verifyLoginStatus();
-	}, []);
+        verifyLoginStatus();
+    }, []);
     
+    /**
+     * Effect hook to fetch image link when the component is focused.
+     */
     useFocusEffect(
         React.useCallback(() => {
             if (userDetails && userDetails._id) {
@@ -41,37 +51,42 @@ function UserProfile() {
         }, [userDetails])
     );
     
-	const renderItem = ({ item }) => (
-		<TouchableOpacity activeOpacity={0.3} onPress={item.handler}>
-			<View style={styles.itemButton}>
-				<Text style={[styles.item]}>{item.name}</Text>
-			</View>
-		</TouchableOpacity>
-	);
+    /**
+     * Function to render an item in the user profile options list.
+     * @param {Object} item - The item to render.
+     * @returns {React.Element} The rendered item.
+     */
+    const renderItem = ({ item }) => (
+        <TouchableOpacity activeOpacity={0.3} onPress={item.handler}>
+            <View style={styles.itemButton}>
+                <Text style={[styles.item]}>{item.name}</Text>
+            </View>
+        </TouchableOpacity>
+    );
 
-	const UserProfileOptions = [
-		{
-			name: 'Recipes',
-			handler: () => navigation.navigate('Recipes'),
-		},
-		{
-			name: 'Reminders',
-			handler: () => navigation.navigate('Reminders'),
-		},
-		{
-			name: 'Goals',
-			handler: () => navigation.navigate('Goals'),
-		},
-		{
-			name: 'Awards',
-			handler: () => navigation.navigate('Awards'),
-		},
-	];
+    const UserProfileOptions = [
+        {
+            name: 'Recipes',
+            handler: () => navigation.navigate('Recipes'),
+        },
+        {
+            name: 'Reminders',
+            handler: () => navigation.navigate('Reminders'),
+        },
+        {
+            name: 'Goals',
+            handler: () => navigation.navigate('Goals'),
+        },
+        {
+            name: 'Awards',
+            handler: () => navigation.navigate('Awards'),
+        },
+    ];
 
-	let currentUsername = '';
-	if (userDetails && userDetails.username) {
-		currentUsername = userDetails.username;
-	}
+    let currentUsername = '';
+    if (userDetails && userDetails.username) {
+        currentUsername = userDetails.username;
+    }
 
 return (
     <View style={styles.container}>
