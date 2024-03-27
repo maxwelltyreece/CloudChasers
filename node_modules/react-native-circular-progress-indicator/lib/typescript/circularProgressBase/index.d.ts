7eabb060e904237cc64b44f6e3707cf38be3f36a
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-03-27 19:56:11 +0000

    fix: remove node modules from repo

--- node_modules/react-native-circular-progress-indicator/lib/typescript/circularProgressBase/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import type { CircularProgressBaseProps, ProgressRef } from '../types';
-declare const CircularProgressBase: React.ForwardRefExoticComponent<CircularProgressBaseProps & React.RefAttributes<ProgressRef>>;
-export default CircularProgressBase;
--- node_modules/react-native-circular-progress-indicator/lib/typescript/components/circleGradient/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import type { CircleGradientProps } from '../../types';
-declare const _default: React.NamedExoticComponent<CircleGradientProps>;
-export default _default;
